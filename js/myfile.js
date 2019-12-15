670ec89736aa2c70f395ebd96f28d82a575ece36
robellis07 <elllrob@hotmail.com>
2019-12-15 14:38:13 -0500

    \

--- js/myfile.js
+++ js/myfile.js
@@ -1,3 +1,11 @@
 function thisIsMyFile(theLatestVal) {
   console.log(`value passed is ${theLatestVal}`);
 }
+
+function thisIs2ndChange() {
+    // a comment goes here
+}
+
+function ThisIs3rdChange() {
+  // this is going to be the third change
+}
