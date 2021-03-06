619aedd3413a745ceeaa9b7353be10ad8a4a8622
Prathamesh Yawalkar <pdyawalkar@gmail.com>
2021-03-06 14:31:03 +0100

    y changed

--- sub/subtraction.py
+++ sub/subtraction.py
@@ -1,6 +1,6 @@
 Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> x = 20
+>>> x = 200
 >>> y = 10
 >>> z = x - y
 >>> z
